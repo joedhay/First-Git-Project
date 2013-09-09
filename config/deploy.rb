4535384858dc6f34f3bae7a65605cc70e1f74c8f
Joener <joedhay@gmail.com>
2013-09-09 16:49:33 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,20 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "First-Git-Project"
+set :repository,  "https://github.com/joedhay/First-Git-Project"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+
+set :user, "joedhay"
+set :use_sudo, false
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :remote_cache
+
+
+role :web, "First-Git-Project"                          # Your HTTP server, Apache/etc
+role :app, "First-Git-Project"                          # This may be the same as your `Web` server
+role :db,  "First-Git-Project", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +23,18 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
+
+namespace :deploy do
+   task :bundle_gems do
+      run "cd #{deploy_to}/current && bundle install vendor/gems"
+   end
+
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run " touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
